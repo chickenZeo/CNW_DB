672516afd99430d24f9e449332c38442d537c4f8
D.Quân <manhquan0105@gmail.com>
2020-06-06 08:30:49 +0700

    Sua database

--- DB.sql
+++ DB.sql
@@ -4,39 +4,40 @@ use PKOT
 go
 
 create table LoaiHang(
-	MaLH int primary key identity(1,1),
+	MaLH char(10) primary key,
 	TenLH nvarchar(50),
 	MoTa ntext,
-	Images varchar(50)
+	Images nvarchar(max)
 )	
 go
 
 create	table NCC(
-	MaNCC int primary key identity(1,1),
+	MaNCC char(10) primary key,
 	TenNCC nvarchar(50),
 	DiaChi nvarchar(100),
 	SDT varchar(20),
-	Email varchar(100)
+	Email varchar(100),
+	Logo nvarchar(max)
 )	
 go
 
 create table SanPham(
-	MaSP int primary key identity(1,1),
+	MaSP char(10) primary key ,
 	TenSP nvarchar(50),
 	SoLuong int,
 	DonGia money,
 	MoTa ntext,
 	GiaKM money,
-	Images varchar(50),
-	MaLH int references LoaiHang(MaLH),
-	MaNCC int references NCC(MaNCC)
+	Images nvarchar(max),
+	MaLH char(10) references LoaiHang(MaLH),
+	MaNCC char(10) references NCC(MaNCC)
 )
 go
 
 create table KhachHang(
-	MaKH int primary key identity(1,1),
+	MaKH char(10) primary key ,
 	TenKH nvarchar(50),
-	SDT varchar(20),
+	SDT varchar(12),
 	DiaChi nvarchar(50),
 	Account varchar(50),
 	Pass varchar(50),
@@ -46,16 +47,16 @@ create table KhachHang(
 go
 
 create table DatHang(
-	MaDH int primary key identity(1,1),
+	MaDH char(10) primary key,
 	NgayDat date,
 	NgayShip date,
 	TienShip money,
-	MaKH int references KhachHang(MaKH)
+	MaKH char(10) references KhachHang(MaKH) on delete cascade
 )
 go
 
 create table Adminn(
-	MaAD int identity(1,1) primary key,
+	MaAD char(10) primary key,
 	UserName varchar(50),
 	Pass varchar(50),
 	Email varchar(100)
@@ -63,10 +64,17 @@ create table Adminn(
 go
 
 create table TinTuc(
-	MaTT int identity(1,1) primary key,
+	MaTT char(10) primary key,
 	NoiDung ntext,
 	TieuDe nvarchar(200),
 	NgayViet date,
 	TacGia nvarchar(100)
 )
 go
+
+create table BinhLuan(
+	MaBL char(10) primary key,
+	NoiDung ntext,
+	MaSP char(10) references SanPham(MaSP), 
+	MaKH char(10) references KhachHang(MaKH)
+)
